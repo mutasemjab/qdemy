86197b4eb9b026dbef27ead30b353703985f4600
mutasem <mutasemms198@gmail.com>
2025-09-09 21:36:03 +0300

    My changes before merge

---
+++ storage/framework/views/29b9302667272b88d6de23eaba8fa864b086c9c5.php
@@ -0,0 +1,24 @@
+
+
+<?php $__env->startSection('title', __('messages.International Program')); ?>
+
+<?php $__env->startSection('content'); ?>
+<section class="gprograms-page">
+    <!-- International -->
+    <a href="#" class="gprogram-card gprogram-card-mixed gprogram-card-main">
+        <span><?php echo e(__('messages.International Program')); ?></span>
+    </a>
+
+    <?php if($programms && $programms->count()): ?>
+    <?php $__currentLoopData = $programms; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $programm): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <a href="<?php echo e(route('international-programm',['programm'=>$programm->id,'slug'=>$programm->slug])); ?>" class="gprogram-card gprogram-card-american">
+        <i class='flag <?php echo e($programm->icon); ?>'></i>
+        <!-- <img data-src="<?php echo e($programm->photo_url); ?>" alt="American Flag"> -->
+        <span><?php echo e($programm->localized_name); ?>cgg</span>
+    </a>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    <?php endif; ?>
+</section>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\qdemy\resources\views/web/gprograms.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/cab6a4d508282739bbec194ae2bd60ef0423750e.php
@@ -0,0 +1,61 @@
+<!-- Header Section -->
+<header class="header">
+    <div class="container">
+        <a href="<?php echo e(route('home')); ?>">
+            <div class="logo">
+                <img src="<?php echo e(asset('assets_front/images/logo.png')); ?>" alt="Logo">
+            </div>
+        </a>
+        <nav class="nav" id="navMenu">
+            <ul>
+                <li><a href="<?php echo e(route('home')); ?>" class="active"><?php echo e(__('front.home')); ?></a></li>
+                <li><a href="<?php echo e(route('contacts')); ?>"><?php echo e(__('front.about_us')); ?></a></li>
+                <li><a href="<?php echo e(route('courses')); ?>"><?php echo e(__('front.courses')); ?></a></li>
+                <li><a href="<?php echo e(route('teachers')); ?>"><?php echo e(__('front.teachers')); ?></a></li>
+                <li><a href="<?php echo e(route('sale-point')); ?>"><?php echo e(__('front.sale_points')); ?></a></li>
+                <li>
+                    <a href="<?php echo e(route('community')); ?>" class="community-link">
+                        <span><?php echo e(__('front.community')); ?></span>
+                        <img src="<?php echo e(asset('assets_front/images/icons/community.png')); ?>" alt="Community Icon" class="icon">
+                    </a>
+                </li>
+                <li><a href="<?php echo e(route('exam.index')); ?>"><?php echo e(__('front.Exams')); ?></a></li>
+                <li><a href="<?php echo e(route('download')); ?>"><?php echo e(__('front.Our Apps')); ?></a></li>
+            </ul>
+        </nav>
+
+        <div class="actions">
+
+            <!-- Cart -->
+            <a href="<?php echo e(route('checkout')); ?>" class="cart-icon">
+                <i class="fas fa-shopping-cart"></i>
+            </a>
+
+            <!-- Auth Buttons -->
+            <?php if(!auth('user')?->user()): ?>
+              <a href="<?php echo e(route('user.register')); ?>" class="btn btn-primary"><?php echo e(__('front.create_account')); ?></a>
+              <a href="<?php echo e(route('user.login')); ?>" class="btn btn-outline"><?php echo e(__('front.login')); ?></a>
+            <?php else: ?>
+              <a href="<?php echo e(route('student.account')); ?>" class="btn btn-outline"><?php echo e(__('front.my_account')); ?></a>
+              <form action="<?php echo e(route('user.logout')); ?>" method='post'><?php echo csrf_field(); ?>
+                <button style='border: 1px solid #0055D2;padding: 8px;' class="btn btn-primary"><?php echo e(__('front.logout')); ?></button>
+              </form>
+            <?php endif; ?>
+
+            <!-- Language Dropdown -->
+            <div class="lang-dropdown">
+                <button class="lang-btn"><i class="fas fa-globe"></i></button>
+                <div class="lang-menu">
+                    <a href="<?php echo e(LaravelLocalization::getLocalizedURL('ar')); ?>">العربية</a>
+                    <a href="<?php echo e(LaravelLocalization::getLocalizedURL('en')); ?>">English</a>
+                </div>
+            </div>
+
+            <!-- Hamburger Button -->
+            <button class="menu-toggle" id="menuToggle">
+                <i class="fas fa-bars"></i>
+            </button>
+        </div>
+    </div>
+</header>
+<?php /**PATH C:\xampp\htdocs\qdemy\resources\views/layouts/header.blade.php ENDPATH**/ ?>
